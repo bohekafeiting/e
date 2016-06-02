6d35376b45ca3393d53c8e678be7ab2af5929e0b
bihailantian <1363896952@qq.com>
2016-06-02 16:35:58 +0800

    Signed-off-by: bihailantian <1363896952@qq.com>

---
+++ thousands_separators-master/src/separators.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function thousands_separators(num) {
+   num += '';
+   x = a.split('.');
+   x1 = x[0]; 
+   x2 = x.length > 1 ? '.' + x[1] : ''; 
+   var rgx = /(\d+)(\d{3})/;
+   while (rgx.test(x1))
+    { 
+	   x1 = x1.replace(rgx, '$1' + ',' + '$2'); 
+	 }
+  return x1 + x2;
+}
+
+module.exports = thousands_separators;

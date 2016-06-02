89eeae1ff686853d942acd7944a5d8a17eda5aa4
bohekafeiting <1363896952@qq.com>
2016-06-02 21:33:24 +0800

    Update calculate-median.js

--- src/calculate-median.js
+++ src/calculate-median.js
@@ -1,11 +1,14 @@
 function calculate_median(arr) {
-   var ave=0;
+   var ave=0,num=0;
    for(i=0;i<arr.length;i++)
      {
 	    if((i+1)%2==0)
-		  ave+=arr[i];
+	      {
+	      	ave+=arr[i];
+	      	num+=1;
+	      }
 	 }
-   return (parseFloat(ave/2));	 
+   return (parseFloat(ave/num));	 
 }
 
 module.exports = calculate_median;

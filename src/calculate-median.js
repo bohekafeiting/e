8e8a5747100a615706da31008846ebbf9282ef81
bihailantian <1363896952@qq.com>
2016-06-02 21:18:49 +0800

    td

--- src/calculate-median.js
+++ src/calculate-median.js
@@ -2,10 +2,10 @@ function calculate_median(arr) {
    var ave=0;
    for(i=0;i<arr.length;i++)
      {
-	    if(a(i+1)%2==0)
+	    if((i+1)%2==0)
 		  ave+=arr[i];
 	 }
-   return (ave/2);	 
+   return (parseFloat(ave/2));	 
 }
 
 module.exports = calculate_median;
